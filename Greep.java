057e44cf114f7e52baedec11bbdb29876b5d6d52
declangibbens <60672327+declangibbens@users.noreply.github.com>
2020-03-10 13:46:45 -0700

    Greeps started

--- Greep.java
+++ Greep.java
@@ -1,51 +1,103 @@
-import greenfoot.*;
-
-/**
- * A Greep is an alien creature that likes to collect tomatoes.
- * 
- * @author (your name here)
- * @version 0.2
- */
-public class Greep extends Creature
-{
-    // Remember: you cannot extend the Greep's memory. So:
-    // no additional fields (other than final fields) allowed in this class!
+    import greenfoot.*;
     
     /**
-     * Default constructor for testing purposes.
+     * A Greep is an alien creature that likes to collect tomatoes.
+     * 
+     * @author (your name here)
+     * @version 0.2
      */
-    public Greep()
+    public class Greep extends Creature
     {
-        this(null);
-    }
+        // Remember: you cannot extend the Greep's memory. So:
+        // no additional fields (other than final fields) allowed in this class!
+        public int count;
+        public int x = 0;
+        public int y = 0;
+        /**
+         * Default constructor for testing purposes.
+         */
+        public Greep()
+        {
+            this(null);
+        }
+        
+        /**
+         * Create a Greep with its home space ship.
+         */
+        public Greep(Ship ship)
+        {
+            super(ship);
+        }
+        
     
-    /**
-     * Create a Greep with its home space ship.
-     */
-    public Greep(Ship ship)
-    {
-        super(ship);
-    }
-
-    /**
-     * Do what a greep's gotta do.
-     */
-    public void act()
-    {
-        super.act();   // do not delete! leave as first statement in act().
-        if (carryingTomato()) {
-            if (atShip()) {
-                dropTomato();
+        /**
+         * Do what a greep's gotta do.
+         */
+        public void act()
+        {
+            super.act();   // do not delete! leave as first statement in act().
+            
+            if (carryingTomato()) {
+                if (atShip()) {
+                    dropTomato();
+                    count++;
+                }
+                else {
+                    if (atWater())
+                    {
+                        turn(50);
+                        move();
+                    }
+                    else
+                    {
+                        turnHome();
+                        move();
+                        spit("red");
+                        
+                    }
+                }
             }
             else {
-                turnHome();
-                move();
+                if (count > 0 & carryingTomato() !=true )
+                {
+                    
+                    if (atWater())
+                    {
+                        turn(40);
+                        move();
+                    }
+                    else
+                    {
+                        turnTowards(x ,y);
+                        move();
+                    }
+                    if (isAtEdge())
+                    {
+                        turn(10);
+                    }
+                }
+                
+                else
+                {
+                    move();
+                    checkFood();
+                    if (atWater())
+                    {
+                        turn(10);
+                    }
+                    if (isAtEdge())
+                    {
+                        turn(10);
+                    }
             }
         }
-        else {
-            move();
-            checkFood();
-        }
+       
+    }
+    
+    public void avoidObstacle()
+    {
+        turn(10);
+        move();
     }
     
     /**
@@ -59,15 +111,18 @@ public class Greep extends Creature
             loadTomato();
             // Note: this attempts to load a tomato onto *another* Greep. It won't
             // do anything if we are alone here.
+            x = getX();
+            y = getY();
         }
     }
+    
 
     /**
      * This method specifies the name of the author (for display on the result board).
      */
     public static String getAuthorName()
     {
-        return "Anonymous";  // write your name here!
+        return "Declan Gibbens";  // write your name here!
     }
 
     /**
